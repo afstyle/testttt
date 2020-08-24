35ebc330ce6e7b124f15c50a68756a250f54db82
afstyle <807652085@qq.com>
2020-08-24 10:57:14 +0800

    update 【更改读取xml的方式】

--- generator/src/main/java/com/huanghao/generator/server/ServerGenerator.java
+++ generator/src/main/java/com/huanghao/generator/server/ServerGenerator.java
@@ -4,7 +4,11 @@ import com.huanghao.generator.util.DbUtil;
 import com.huanghao.generator.util.Field;
 import com.huanghao.generator.util.FreemarkerUtil;
 import freemarker.template.TemplateException;
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
@@ -18,13 +22,31 @@ public class ServerGenerator {
     static String toServicePath = "server\\src\\main\\java\\com\\huanghao\\server\\service\\";
     static String toDTOPath = "server\\src\\main\\java\\com\\huanghao\\server\\dto\\";
     static String toControllerPath = String.format("%s\\src\\main\\java\\com\\huanghao\\%s\\controller\\admin\\", MODULE, MODULE);
+    static String generatorConfigPath = "server\\src\\main\\resources\\generator\\generatorConfig.xml";
 
     public static void main(String[] args) throws Exception {
-        String Domain = "Section";
-        String domain = "section";
-        String tableNameCn = "小节";
+        // 只生成配置文件中的第一个table节点
+        File file = new File(generatorConfigPath);
+        SAXReader reader=new SAXReader();
+        //读取xml文件到Document中
+        Document doc=reader.read(file);
+        //获取xml文件的根节点
+        Element rootElement=doc.getRootElement();
+        //读取context节点
+        Element contextElement = rootElement.element("context");
+        //定义一个Element用于遍历
+        Element tableElement;
+        //取第一个“table”的节点
+        tableElement = contextElement.elementIterator("table").next();
+        String Domain = tableElement.attributeValue("domainObjectName");
+        String tableName = tableElement.attributeValue("tableName");
+        String tableNameCn = DbUtil.getTableComment(tableName);
+        String domain = Domain.substring(0, 1).toLowerCase() + Domain.substring(1);
+        System.out.println("表："+tableElement.attributeValue("tableName"));
+        System.out.println("Domain："+tableElement.attributeValue("domainObjectName"));
 
-        List<Field> fieldList = DbUtil.getColumnByTableName(domain);
+
+        List<Field> fieldList = DbUtil.getColumnByTableName(tableName);
         Set<String> typeSet = getJavaTypes(fieldList);
         Map<String, Object> map = new HashMap<>();
         map.put("Domain", Domain);
